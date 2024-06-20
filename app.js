3cb9f1ad626ec5bae4ee840c47496105400c96cf
Ali Jahankah <aral2@yahoo.com>
2024-06-20 19:29:33 +0100

    feat - connect to DB and run data handler

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const express = require('express'); // app.js
+const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const routes = require('./routes/index');
 const { rootRoute } = require('./constants/routes');
 const db = require('./config/db');
+const { ingestDataHandler } = require('./utils/dataHandler');
 
 const app = express();
 const PORT = process.env.PORT || 4001;
 app.get(rootRoute.url, routes);
 app.listen(PORT, () => {
   console.log(`\n \n server running at on port ==> ${PORT} \n`);
+  db.once('open', async () => {
+    console.log('!!!! DB Connected !!!!!');
+
+    try {
+      await ingestDataHandler();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      mongoose.connection.close();
+      console.log('!!!! DB Disconnected !!!!!');
+    }
+  });
 });
 module.exports = app;
