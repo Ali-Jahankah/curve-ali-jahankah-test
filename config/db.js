4d2819775954b98d655a8b2e7fa8ffec43e22098
Ali Jahankah <aral2@yahoo.com>
2024-06-20 19:31:05 +0100

    fix - move db connection to app.js

--- config/db.js
+++ config/db.js
@@ -6,6 +6,5 @@ const Contract = require('../models/contract');
 mongoose.connect(dbUri);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => console.log('--- DB connected! ---'));
 
 module.exports = db;
