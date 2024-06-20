67be9f9eadc7ab84047eb7801c687c222716cc00
Ali Jahankah <aral2@yahoo.com>
2024-06-20 10:55:44 +0100

    fix - tidying app.js codes

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express'); // app.js
-const app = express();
 require('dotenv').config();
-const PORT = process.env.PORT || 4001;
+
 const routes = require('./routes/index');
 const { rootRoute } = require('./constants/routes');
-app.get(rootRoute.url, require('./routes/index'));
+const db = require('./config/db');
 
+const app = express();
+const PORT = process.env.PORT || 4001;
+app.get(rootRoute.url, routes);
 app.listen(PORT, () => {
-  console.log(`server running at on port ==> ${PORT}`);
+  console.log(`\n \n server running at on port ==> ${PORT} \n`);
 });
 module.exports = app;
