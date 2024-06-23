019bf3a7dc37db79c4ec89ab06c4da72b83b68be
Ali Jahankah <aral2@yahoo.com>
2024-06-23 14:27:03 +0100

    feat - create test env

--- app.js
+++ app.js
@@ -3,26 +3,44 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const routes = require('./routes/index');
-const { rootRoute } = require('./constants');
-const db = require('./config/db');
-const { ingestDataHandler } = require('./utils/dataHandler');
+const { rootRoute, dbUri } = require('./constants');
+const { extractedRows } = require('./utils/dataHandler');
+const { connectDb } = require('./config/db');
 
 const app = express();
-const PORT = process.env.PORT || 4001;
-app.get(rootRoute.url, routes);
-app.listen(PORT, () => {
-  console.log(`\n \n server running at on port ==> ${PORT} \n`);
-  db.once('open', async () => {
-    console.log('!!!! DB Connected !!!!!');
-
-    try {
-      await ingestDataHandler();
-    } catch (error) {
-      console.log(error);
-    } finally {
-      mongoose.connection.close();
-      console.log('!!!! DB Disconnected !!!!!');
-    }
+const startServer = async () => {
+  try {
+    await connectDb(dbUri);
+    await extractedRows();
+    const PORT = process.env.PORT || 4001;
+    const server = app.listen(PORT, () => {
+      console.log(`\n \n server running at on port ==> ${PORT} \n`);
+    });
+    process.on('SIGINT', async () => {
+      try {
+        await mongoose.connection.close();
+        console.log('DB disconnected via app termination');
+        server.close(() => {
+          console.log('Server stopped via app termination');
+          process.exit(0);
+        });
+      } catch (err) {
+        console.error('Error in closing MongoDB connection:', err);
+        process.exit(1);
+      }
+    });
+    return server;
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+if (process.env.NODE_ENV !== 'test') {
+  startServer().catch((err) => {
+    console.error('Error starting the server:', err);
+    process.exit(1);
   });
-});
+}
+app.get(rootRoute.url, routes);
+
 module.exports = app;
