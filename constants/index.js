d44a5724abacd6314bd3535b912cad463f4b38ed
Ali Jahankah <aral2@yahoo.com>
2024-06-23 14:28:22 +0100

    feat - add test DB uri

--- constants/index.js
+++ constants/index.js
@@ -3,6 +3,6 @@ exports.rootRoute = { url: '/', message: 'Hello World' };
 
 // DB Uri
 exports.dbUri = 'mongodb://127.0.0.1:27017/curveDB';
-
+exports.dbUriTest = 'mongodb://127.0.0.1:27017/testCurvDB';
 // Contract Name
 exports.contractFiletName = 'Contract 1';
